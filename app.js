b7ead73d69c14c3b87b2021d9293ca0a69bb4e97
Kristijan Karanfiloski <kikaccc28@gmail.com>
2023-02-13 14:59:58 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const panels = document.querySelectorAll(".panel");
+
+function toogleOpen() {
+  this.classList.toggle("open");
+}
+
+function toogleActive(e) {
+  console.log(e.propertyName);
+  if (e.propertyName.includes("flex")) {
+    this.classList.toggle("open-active");
+  }
+}
+
+panels.forEach((panel) => panel.addEventListener("click", toogleOpen));
+panels.forEach((panel) =>
+  panel.addEventListener("transitionend", toogleActive)
+);
